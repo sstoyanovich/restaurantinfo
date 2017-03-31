3be42c0927af1218ee92756e862f1736199935b8
Stefan <stefan.stoyanovich@hbo.com>
2017-03-31 19:55:43 -0400

    test

--- members/job-id-helper-update-inc.php
+++ members/job-id-helper-update-inc.php
@@ -2,19 +2,14 @@
 $query2 = "SELECT job_id,job_title_id,job_title FROM jobs ORDER BY job_id DESC LIMIT 100";
 $result2 = mysql_query($query2) or die(mysql_error());
 
-echo "outside of loop";
+
 while ($rs2 = mysql_fetch_object($result2))
 {
 
 	$job_id = stripslashes($rs2->job_id);
 	$job_title_id = stripslashes($rs2->job_title_id);
 	$job_title = stripslashes($rs2->job_title);
-	echo($job_title_id);
-	echo("<br><br>");
-	echo($job_title);
-	echo("<br><br>");
-	echo($job_id);
-	echo("<br><br>");
+
 
 	$query3 = "SELECT job_id_helper_id FROM job_id_helper WHERE job_id=" . $job_id;
 	$result3 = mysql_query($query3) or die(mysql_error());
@@ -24,11 +19,11 @@ while ($rs2 = mysql_fetch_object($result2))
 	{
 		$query3 = "INSERT INTO job_id_helper SET job_id=" . $job_id . ",job_title_id=" . $job_title_id ;
 		$result3 = mysql_query($query3) or die(mysql_error());
-		echo "inserted new jobs into job id helper";
+
 	}
 	if ($job_title_id)
 	{
-		echo "inside of job title id if";
+
 		$query3 = "SELECT job_title FROM job_titles WHERE job_title_id=" . $job_title_id;
 		$result3 = mysql_query($query3) or die(mysql_error());
 		$rs3 = mysql_fetch_object($result3);
@@ -44,5 +39,4 @@ while ($rs2 = mysql_fetch_object($result2))
 	}
 }
 
-echo "reached end of file";
 @mysql_free_result($result2);
