11a1188995f36c6927ca47823db038a43c47ca7d
Stefan <stefan.stoyanovich@hbo.com>
2017-03-31 19:06:12 -0400

    test

--- members/job-id-helper-update-inc.php
+++ members/job-id-helper-update-inc.php
@@ -2,10 +2,10 @@
 $query2 = "SELECT job_id,job_title_id FROM jobs ORDER BY job_id DESC LIMIT 100";
 $result2 = mysql_query($query2) or die(mysql_error());
 
-echo("outside of loop");
+
 while ($rs2 = mysql_fetch_object($result2))
 {
-	echo("inside of loop");
+
 	$job_id = stripslashes($rs2->job_id);
 	$job_title_id = stripslashes($rs2->job_title_id);
 	echo($job_id + "  " + $job_title_id);
@@ -13,6 +13,7 @@ while ($rs2 = mysql_fetch_object($result2))
 	$query3 = "SELECT job_id_helper_id FROM job_id_helper WHERE job_id=" . $job_id;
 	$result3 = mysql_query($query3) or die(mysql_error());
 	$has_entry = mysql_num_rows($result3);
+	echo($result3);
 	if (!$has_entry)
 	{
 		$query3 = "INSERT INTO job_id_helper SET job_id=" . $job_id;
