3664e1dd2fda8b19150765b8c58e0937cf2da486
Stefan <stefan.stoyanovich@hbo.com>
2017-04-04 18:53:12 -0400

    test

--- upload.php
+++ upload.php
@@ -1,27 +1,23 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-?>
+
 <!DOCTYPE html>
 <html>
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="text" name="first_name" size="40" maxlength="255" value="<?=$first_name?>" onfocus="this.select();">
+    <input type="file" name="fileToUpload" value="<?=$file?>">
+    <?php
+
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    if($imageFileType == "jpg"){
+    echo "jpg";
+  }
+    if($imageFileType == "pdf"){
+    echo "pdf";
+  }
+    ?>
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
