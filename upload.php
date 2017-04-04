8202cec1d130449df913a17294faa65ef311dc31
Stefan <stefan.stoyanovich@hbo.com>
2017-04-04 18:54:50 -0400

    test

--- upload.php
+++ upload.php
@@ -5,12 +5,11 @@
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="text" name="first_name" size="40" maxlength="255" value="<?=$first_name?>" onfocus="this.select();">
     <input type="file" name="fileToUpload" value="<?=$file?>">
     <?php
 
     $uploadOk = 1;
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    $imageFileType = pathinfo($file,PATHINFO_EXTENSION);
     if($imageFileType == "jpg"){
     echo "jpg";
   }
