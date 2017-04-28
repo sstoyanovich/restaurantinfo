bc331c649588c30adc0051541d8c0bcb4617a225
gnr <88@optikjunk.de>
2017-04-27 22:54:57 -0500

    fine tune header styles

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 session_start();
 $use_CDN = 1;
 $this_page = "home";
-ini_set('error_reporting', E_ALL);
-error_reporting(E_ALL);
+//ini_set('error_reporting', E_ALL);
+//error_reporting(E_ALL);
 
 require("bootstrap_v1/incld/config.php");
 require("bootstrap_v1/incld/utils.php");
@@ -15,5 +15,6 @@ require("bootstrap_v1/incld/page_start_inc.php");
     <? //require("members/search-form-new.php"); ?>
     <? require("bootstrap_v1/incld/footer_inc.php"); ?>
 </div><!--/ .home-->
+<div style="width: 100%; height: 1450px; background: transparent url(/images/overlay/home-page.png) no-repeat 50% 0; opacity: .3; position: absolute; top: 0; left: 0; z-index: 100000000; display: none;"></div>
 <?
 require("bootstrap_v1/incld/page_end_inc.php");
