36c97d3fbea1cfaffd380a563f02341efc4d4d22
gnr <88@optikjunk.de>
2017-05-30 23:49:12 -0500

    add markup for jobs to job page

--- bootstrap_v1/content/jobs-inc.php
+++ bootstrap_v1/content/jobs-inc.php
@@ -1,28 +1,31 @@
 <?
-$query = "SELECT
-                *
-            FROM 
-                jobs 
-            WHERE 
-                jobs.expired=0";
+$query = "SELECT job_titles.job_title, jobs.description, jobs.meta_description,jobs.contact_company, jobs.city, jobs.state, members.company_culture from jobs, job_titles, members WHERE job_titles.job_title_id = jobs.job_title_id AND members.member_id = jobs.member_id AND jobs.description IS NOT NULL ORDER BY jobs.date_listed DESC LIMIT 7";
 
 $result = mysql_query($query) or die(mysql_error());
 ?>
-<pre>
-<?  var_dump($result);
-?>   
-</pre>
+<ul id="jobs-listing">
+
+<?  while ($rs = mysql_fetch_object($result)): ?>
+    <li>
+        <dl>
+            <dt><? echo $rs->job_title; ?></dt>
+            <dd><? echo $rs->contact_company . " &mdash; " . $rs->city . ", " . $rs->state; ?>
+            </dd>
+            <dd class="long-description">
+                <h3>Short Description</h3>
+                <p><? echo $rs->description; ?>
+                </p>
+                <h3>Company Culture</h3>
+                <p><? echo $rs->company_culture; ?>
+                </p>
+                <h3>Job Details</h3>
+                <p><? echo $rs->meta_description; ?>
+                </p>
+            </dd>
+        </dl>
+    </li>
+<?  endwhile; ?>
+</ul><!--/ .jobs-listing-->
 <?
-    while ($rs = mysql_fetch_object($result)) {
-?>
-<pre>
-<?
-        var_dump($rs);
-?>
-</pre>
-<?
-  }
-?>
-<?    
     mysql_free_result($result)
 ?>
